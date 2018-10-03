bea72e846050db19e36ac0b301e6e39b5a160df6
Yu Ting Yang <yutingyang@yus-macbook-pro.local>
2018-10-03 12:00:26 -0400

    Initial Commit

---
+++ Lab1_TumblrTests/Lab1_TumblrTests.swift
@@ -0,0 +1,36 @@
+//
+//  Lab1_TumblrTests.swift
+//  Lab1_TumblrTests
+//
+//  Created by Yu Ting Yang on 10/3/18.
+//  Copyright © 2018 Yu Ting Yang. All rights reserved.
+//
+
+import XCTest
+@testable import Lab1_Tumblr
+
+class Lab1_TumblrTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
